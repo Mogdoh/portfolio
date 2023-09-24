190832b82fc6556caf35db41ee716bda3931a1ef
Mogdoh <snnsung23@gmail.com>
2023-09-24 19:29:52 +0900

    feat: add home page

---
+++ src/components/start/profilepage.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <li>내 사진 혹은 내 이미지를 나타낼 수 있는 그림 위치</li>
+            
+            <li>User 혹은 내 닉네임</li>
+        
+            <li>환영합니다</li>
+            
+            <ul>
+                <li>Profile 항목 1</li>
+                <li>Profile 항목 2</li>
+                <li>Profile 항목 3</li>
+                <li>Profile 항목 4</li>
+            </ul>
+        </div>
+    );
+};
+
+export default ProfilePage;
